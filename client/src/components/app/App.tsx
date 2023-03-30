5d02c5f82fb25de14fdb86d4a8d86877b286334d
Manu <le.marques.emanuel@gmail.com>
2023-03-30 10:47:06 +0200

    added css for login

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Login from '../login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
    <h1>hello</h1>
+   <Login />
     </div>
   )
 }
--- client/src/components/login/index.tsx
+++ client/src/components/login/index.tsx
@@ -1,7 +1,25 @@
-export default function Login () {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
+import * as React from 'react';
+import './login.css';
+
+export default function Login() {
+    return (
+        <div className="login">
+            <div className="login__signup">
+                <button>Se connecter</button>
+            </div>
+            <div className="login__register">
+                <button>S'inscrire</button>
+            </div>
+            <div className="login__forgot">
+                <button>Mot de passe oublié</button>
+            </div>
+            <div className="login__form">
+                <form>
+                    <input type="text" placeholder="Email" />
+                    <input type="password" placeholder="Mot de passe" />
+                    <button>Se connecter</button>
+                </form>
+                </div>
+        </div>
+    )
 }
