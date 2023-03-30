09cfeea86e2efd514e583a0911649a1d81626305
Manu <le.marques.emanuel@gmail.com>
2023-03-30 14:28:35 +0200

    design login page

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -6,7 +6,6 @@ function App() {
 
   return (
     <div className="App">
-   <h1>hello</h1>
    <Login />
     </div>
   )
--- client/src/components/login/index.tsx
+++ client/src/components/login/index.tsx
@@ -4,22 +4,21 @@ import './login.css';
 export default function Login() {
     return (
         <div className="login">
-            <div className="login__signup">
-                <button>Se connecter</button>
+            <div className="login__title">
+                <h1>Login</h1>
             </div>
-            <div className="login__register">
-                <button>S'inscrire</button>
+            <div className="login__form">
+                <form>
+                    <h1 className='login__form__email'>Email</h1>
+                    <input type="text" placeholder="Email" />
+                    <h1 className='login__form__mdp'>Mot de passe</h1>
+                    <input type="password" placeholder="Mot de passe" />
+                    <button className='login__form__button'>Se connecter</button>
+                </form>
             </div>
             <div className="login__forgot">
                 <button>Mot de passe oublié</button>
             </div>
-            <div className="login__form">
-                <form>
-                    <input type="text" placeholder="Email" />
-                    <input type="password" placeholder="Mot de passe" />
-                    <button>Se connecter</button>
-                </form>
-                </div>
         </div>
     )
 }
