5f3e75d06b1cda0d449de01f3da6526fa86339df
Manu <le.marques.emanuel@gmail.com>
2023-03-30 09:58:15 +0200

    added login component

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-   
+   <h1>hello</h1>
     </div>
   )
 }
