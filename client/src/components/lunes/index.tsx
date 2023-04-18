6aa786ee17a15a436785ca9f60cf93bec1cefcb8
Manu <le.marques.emanuel@gmail.com>
2023-04-18 14:56:27 +0200

    display planetes and moons list

--- client/src/components/lunes/index.tsx
+++ client/src/components/lunes/index.tsx
@@ -1,9 +1,41 @@
+import './styles.scss'
+
 export default function Lunes() {
     return (
         <div className="lunes">
-        <div className="lunes__title">
-            <h1>Les Lunes</h1>
-        </div>
+            <h1 className='lunes__title'>Les Lunes</h1>
+            <div className="lunes__container">
+                <div className="lunes__container__main">
+                    <h2 className="lunes__container__orbite__title">Lunes</h2>
+                    <p className="lunes__container__orbite__description">lunes est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="lunes__container__orbite__image" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg" alt="lunes" />
+                    <a className="lunes__container__orbite__link" href="https://fr.wikipedia.org/wiki/lunes_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+                <div className="lunes__container__main">
+                    <h2 className="lunes__container__orbite__title">lunes</h2>
+                    <p className="lunes__container__orbite__description">lunes est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="lunes__container__orbite__image" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg" alt="lunes" />
+                    <a className="lunes__container__orbite__link" href="https://fr.wikipedia.org/wiki/lunes_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+                <div className="lunes__container__main">
+                    <h2 className="lunes__container__orbite__title">lunes</h2>
+                    <p className="lunes__container__orbite__description">lunes est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="lunes__container__orbite__image" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg" alt="lunes" />
+                    <a className="lunes__container__orbite__link" href="https://fr.wikipedia.org/wiki/lunes_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+                <div className="lunes__container__main">
+                    <h2 className="lunes__container__orbite__title">lunes</h2>
+                    <p className="lunes__container__orbite__description">lunes est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="lunes__container__orbite__image" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg" alt="lunes" />
+                    <a className="lunes__container__orbite__link" href="https://fr.wikipedia.org/wiki/lunes_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+                <div className="lunes__container__main">
+                    <h2 className="lunes__container__orbite__title">lunes</h2>
+                    <p className="lunes__container__orbite__description">lunes est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="lunes__container__orbite__image" src="https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg" alt="lunes" />
+                    <a className="lunes__container__orbite__link" href="https://fr.wikipedia.org/wiki/lunes_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+            </div>
         </div>
     );
     }
--- client/src/components/planetes/index.tsx
+++ client/src/components/planetes/index.tsx
@@ -3,9 +3,39 @@ import './styles.scss'
 export default function Planetes() {
     return (
         <div className="planetes">
-        <div className="">
             <h1 className='planetes__title'>Les Planètes</h1>
-        </div>
+            <div className="planetes__container">
+                <div className="planetes__container__main">
+                    <h2 className="planetes__container__mercury__title">Mercure</h2>
+                    <p className="planetes__container__mercury__description">Mercure est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="planetes__container__mercury__image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/1200px-Mercury_in_color_-_Prockter07-edit1.jpg" alt="mercury" />
+                    <a className="planetes__container__mercury__link" href="https://fr.wikipedia.org/wiki/Mercure_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+                <div className="planetes__container__main">
+                    <h2 className="planetes__container__mercury__title">Mercure</h2>
+                    <p className="planetes__container__mercury__description">Mercure est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="planetes__container__mercury__image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/1200px-Mercury_in_color_-_Prockter07-edit1.jpg" alt="mercury" />
+                    <a className="planetes__container__mercury__link" href="https://fr.wikipedia.org/wiki/Mercure_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+                <div className="planetes__container__main">
+                    <h2 className="planetes__container__mercury__title">Mercure</h2>
+                    <p className="planetes__container__mercury__description">Mercure est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="planetes__container__mercury__image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/1200px-Mercury_in_color_-_Prockter07-edit1.jpg" alt="mercury" />
+                    <a className="planetes__container__mercury__link" href="https://fr.wikipedia.org/wiki/Mercure_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+                <div className="planetes__container__main">
+                    <h2 className="planetes__container__mercury__title">Mercure</h2>
+                    <p className="planetes__container__mercury__description">Mercure est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="planetes__container__mercury__image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/1200px-Mercury_in_color_-_Prockter07-edit1.jpg" alt="mercury" />
+                    <a className="planetes__container__mercury__link" href="https://fr.wikipedia.org/wiki/Mercure_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+                <div className="planetes__container__main">
+                    <h2 className="planetes__container__mercury__title">Mercure</h2>
+                    <p className="planetes__container__mercury__description">Mercure est la planète la plus proche du Soleil et la plus petite du Système solaire.</p>
+                    <img className="planetes__container__mercury__image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/1200px-Mercury_in_color_-_Prockter07-edit1.jpg" alt="mercury" />
+                    <a className="planetes__container__mercury__link" href="https://fr.wikipedia.org/wiki/Mercure_(plan%C3%A8te)">En savoir plus</a>
+                </div>
+            </div>
         </div>
     );
-    }
+}
