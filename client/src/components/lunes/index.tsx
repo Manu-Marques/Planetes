d4e6af30ce2c7f0ef2bba438b7f8c8f4e2052afc
Manu <le.marques.emanuel@gmail.com>
2023-04-26 17:07:30 +0200

    trying to add images from the back

--- client/src/components/lunes/index.tsx
+++ client/src/components/lunes/index.tsx
@@ -3,63 +3,65 @@ import './styles.scss'
 import { useState, useEffect } from 'react';
 
 interface CardProps {
-  id: number;
-  title: string;
-  description: string;
+    id: number;
+    name: string;
+    description: string;
+    img: string;
 }
 
-function Card({ id, title, description }: CardProps) {
-  const [angleX, setAngleX] = useState<number>(0);
-  const [angleY, setAngleY] = useState<number>(0);
-  const [scale, setScale] = useState<number>(1);
+function Card({ id, name, description, img }: CardProps) {
+    const [angleX, setAngleX] = useState<number>(0);
+    const [angleY, setAngleY] = useState<number>(0);
+    const [scale, setScale] = useState<number>(1);
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    const elRect = e.currentTarget.getBoundingClientRect();
-    const x = e.clientX - elRect.x;
-    const y = e.clientY - elRect.y;
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        const elRect = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - elRect.x;
+        const y = e.clientY - elRect.y;
 
-    const midCardWidth = elRect.width / 2;
-    const midCardHeight = elRect.height / 2;
+        const midCardWidth = elRect.width / 2;
+        const midCardHeight = elRect.height / 2;
 
-    setAngleY((x - midCardWidth) / 8);
-    setAngleX((y - midCardHeight) / 8);
-    setScale(1.1);
-  };
-
-  const handleMouseLeave = () => {
-    setAngleX(0);
-    setAngleY(0);
-    setScale(1);
+        setAngleY((x - midCardWidth) / 8);
+        setAngleX((y - midCardHeight) / 8);
+        setScale(1.1);
     };
 
-  return (
-    <div className="cards" >
-      <div className='card' onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{ transform: `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(${scale})` }}>
-        <h2>{title}</h2>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+    const handleMouseLeave = () => {
+        setAngleX(0);
+        setAngleY(0);
+        setScale(1);
+    };
+
+    return (
+        <div className="cards" >
+            <div className='card' onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{ transform: `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(${scale})` }}>
+                <h2>{name}</h2>
+                <p>{description}</p>
+                <img src={img}>{img}</img>
+            </div>
+        </div>
+    );
 }
 
 function CardList() {
-  const [cards, setCards] = useState<CardProps[]>([]);
+    const [cards, setCards] = useState<CardProps[]>([]);
 
     useEffect(() => {
         fetch('http://localhost:3000/cards')
             .then(response => response.json())
             .then(data => setCards(data));
     }, []);
-  
 
 
-  return (
-    <div>
-      {cards.map(card => (
-        <Card key={card.id} id={card.id} title={card.title} description={card.description} />
-      ))}
-    </div>
-  );
+
+    return (
+        <div>
+            {cards.map(card => (
+                <Card key={card.id} id={card.id} name={card.name} description={card.description} img={card.img} />
+            ))}
+        </div>
+    );
 }
 
 export default CardList;
